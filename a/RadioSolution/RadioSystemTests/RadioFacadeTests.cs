2c2a6490ebda742e7279bdf8b48704461bcc1a9f
Arlen Rios <arlenrios@icloud.com>
2015-10-22 10:07:25 +0200

    Allt inför tentan

---
+++ a/RadioSolution/RadioSystemTests/RadioFacadeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RadioSystemTests
+{
+    [TestClass]
+    public class RadioFacadeTests
+    {
+        [TestMethod]
+        public void Test_That_Facade_Can_Be_Created()
+        {
+            RadioFacade facade = new RadioFacade();
+            Assert.IsNotNull(facade);
+        }
+        [TestMethod]
+        public void Test_That_RixFM_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+
+            facade.PlayMusic("MixMegapol");
+            Assert.IsTrue(facade.Is)
+        }
+        [TestMethod]
+        public void Test_That_P3_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+
+            facade.PlayMusic("P3");
+            Assert.IsTrue(facade.Is)
+        }
+    }
+}
